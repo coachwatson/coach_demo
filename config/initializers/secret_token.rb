c954e3affec7aec71e9759f3c703a9671b66ed78
Coach <coachwatson@gmail.com>
2013-01-11 14:01:37 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoachDemo::Application.config.secret_token = '96535dd153d687d3f00dcc4ef846c2bf57281b898152dd3e7c3c8a2170d4abf54cebf780fa04266a93c33d4ec2b0a9ab8b8f53934aa8dcc4eefdab5d2d31d726'
